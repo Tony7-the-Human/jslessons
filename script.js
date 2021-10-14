be9a30dc30016ef45d1dc787a4852259c0c3a3fe
Tony7-the-Human <mrfox88@yandex.ru>
2021-10-14 19:34:33 +0300

    add homework for lesson 19

--- script.js
+++ script.js
@@ -1,129 +1,175 @@
 "use strict";
-// function calculateArea(r){
-//     let area;
-//     if(r<=0){
-//         return 0;
-//     }else{
-//         area = Math.PI * r *r;
-//         return area;
-//     }
+// Используя литерал объекта создайте пустой объект cat.
+// Добавьте свойство name со значением "Roxy".
+// Добавьте свойство lags со значением 4.
+// Добавьте свойство color со значением "grey".
+// Добавьте свойство full name со значением "Roxy, красивая кошка".
+// Выведите full name через console.log().
+// let cat = {};
+// cat.name = "Roxy";
+// cat.lags = 4;
+// cat.color = "grey";
+// cat["full name"] = "Roxy, красивая кошка";
+// console.log(cat["full name"]);
+
+// Напишите функцию-конструктор Person, которая принимает два параметра:
+// name (имя) и age (возраст).
+// С помощью оператора new создайте объект person_1, передающий в
+// функцию-конструктор аргументы: имя "Andrey" и возраст 28.
+// Выведите свойства объекта person_1 через console.log().
+// Добавьте к шаблону объекта Person свойство specialty с помощью
+// prototype. Значением свойства будет "менеджер".
+// Выведите свойство specialty объекта person_1 через console.log().
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
 // }
-// let radius = 2.2;
-// let theArea = calculateArea(radius);
-// console.log('The area is: ' + theArea);
+// let person_1 = new Person('Andrey', 28);
+// console.log(person_1.name);
+// console.log(person_1.age);
+// console.log(person_1);
 
-// Дана строка 'abc!def!'. Замените все ! на '@'
-// с помощью глобального поиска и замены.
-// let txt = 'abc!def!';
-// console.log(txt.replace(/!/g, '@'));
+// Person.prototype.specialty = "менеджер";
+// console.log(person_1.specialty);
 
-// Дана строка 'aaa bbb ccc'. Вырежите из нее слово 'bbb'
-// тремя разными способами (через substr, substring, slice).
-// let txt = 'aaa bbb ccc';
-// console.log(txt.substr(4,3));
-// console.log(txt.substring(4,7));
-// console.log(txt.slice(4,7));
+// Используя литерал объекта создайте объект cat с тремя свойствами:
+// name, legs и color.
+// С помощью Object.create() создайте объект kitten
+// объектом-прототипом которого будет cat.
+// Выведите свойства объекта kitten через console.log().
 
-// Дана строка 'js'. Сделайте из нее строку 'JS'.
-// let txt = 'js';
-// console.log(txt.toUpperCase());
-
-// Дана строка 'я люблю JS!'. Найдите количество
-// символов в этой строке.
-let txt = "я люблю JS!";
-console.log(txt.length);
-
-// Домашнее задание
-// Выписать в комментариях. Вам необходимо найти все переменные,
-// функции, аргументы и параметры.
-// Запишите имена в соответсвующие пункты
-// Переменные: years, myDog, guests
-// Функции: dogYears, makeTea, secret
-// Встроенные функции: console.log()
-// Аргументы: myDog, 4, guests, "Earl Grey"
-// Параметры: dogname, age, cups, tea
-// function dogYears(dogname, age) {
-// 	let years = age * 2;
-// 	console.log(dogName + " is " + years + " years old"); // тут ошибка в параметре dogName
+// let cat = {
+//     legs:4,
+//     name : 'Roxy',
+//     color: 'grey'
 // }
-// let myDog = "Rex";
-// dogYears(myDog, 4);
+// let kitten = Object.create(cat);
+// console.log(kitten.name);
+// console.log(kitten.legs);
+// console.log(kitten.color);
 
-// function makeTea(cups, tea) {
-// 	console.log("Breawing " + cups + " cups of" + tea);
+// Реализуйте класс Worker (Работник), который будет иметь следующие
+// свойства: name (имя), surname (фамилия), rate (ставка за день работы),
+// days (количество отработанных дней). Также класс должен иметь метод
+// getSalary(), который будет выводить зарплату работника. Зарплата -
+// это произведение (умножение) ставки rate на количество отработанных
+// дней days.
+
+// class worker {
+// 	constructor(name, surname, rate, days) {
+// 		this.name = name;
+// 		this.surname = surname;
+// 		this.rate = rate;
+// 		this.days = days;
+// 	}
+// 	getSalary() {
+// 		return this.rate * this.days;
+// 	}
 // }
-// let guests = 2;
-// makeTea(guests, "Earl Grey");
+// worker = new worker("Andrey", "Makeenko", 100, 21);
+// console.log(worker.name);
+// console.log(worker.surname);
+// console.log(worker.getSalary());
+//Домашнее задание
+// Создайте объект city1 (var city1 = {}), укажите у него свойства name
+// (название города, строка) со значением «ГородN» и population
+// (населенность города, число) со значением 10 млн.
 
-// function secret(){
-//     console.log('The secret of life is 28');
-// }
-// secret();
+let city1 = {
+	name: "ГородN",
+	population: 10000000,
+};
 
-// Дана строка 'JS'. Сделайте из нее строку 'js'.
+// Создайте объект city2 через нотацию {name: "ГородM", population: 1e6}.
 
-let text1 = "JS";
+let city2 = {
+	name: "ГородM",
+	population: 1e6,
+};
 
-console.log(text1.toLocaleLowerCase());
+// Создайте у объектов city1 и city2 методы getName(), которые вернут
+// соответствующие названия городов.
 
-// Дана строка 'я люблю JS!'. Вырежите из нее слово
-// 'люблю' и слово 'javascript' тремя разными способами
-// (через substr, substring, slice).
-
-let text2 = "я люблю JS!";
-
-console.log(text2.substr(2, 6));
-console.log(text2.substring(2, 7));
-console.log(text2.slice(2, 7));
-
-// Дана строка 'я люблю JS!'. Найдите позицию
-// подстроки 'люблю'.
-
-console.log(text2.indexOf("люблю"));
-
-// Дана переменная txt, в которой хранится какой-либо текст.
-// Реализуйте обрезание длинного текста по следующему
-// принципу: если количество символов этого текста больше
-// заданного в переменной n, то в переменную result запишем
-// первые n символов строки txt и добавим в конец
-// троеточие '...'. В противном случае в переменную
-// result запишем содержимое переменной txt.
-
-function cutString(text, simbolsTxt) {
-	let result = "";
-	if (text.length > simbolsTxt) {
-		result = text.substr(0, simbolsTxt);
-		console.log(result + "...");
-	} else {
-		result = text;
-		console.log(result);
-	}
+function getName() {
+	return this.name;
 }
-let txt3 = "какой-то длинный длинный текст";
-cutString(txt3, 16);
+city1.getName = getName;
+city2.getName = getName;
 
-// Для решения задач данного блока вам понадобятся следующие
-// методы: replace.
-// Дана строка 'Я-люблю-JS!'.
-// Замените все  '-' на '!' с помощью глобального
-// поиска и замены.
+// console.log(city1.getName());
+// console.log(city2.getName());
 
-let txt4 = "Я-люблю-JS!";
+// Создайте методы exportStr() у каждого из объектов. Этот метод должен
+// возвращать информацию о городе в формате
+// «name=ГородN\npopulation=10000000\n». Для второго города будет строка
+// со своими значениями. Примечание: можно обращаться к каждому свойству
+// через цикл for/in, но методы объекта возвращать не нужно.
 
-console.log(txt4.replace(/-/g, "!"));
+function exportStr() {
+	return `name=${this.name}\npopulation=${this.population}\n`;
+}
+city1.exportStr = exportStr;
+city2.exportStr = exportStr;
 
-// Дана строка 'я люблю JS'. С помощью метода split
-// запишите каждое слово этой строки в отдельный элемент
-// массива.
+console.log(city1.exportStr());
+console.log(city2.exportStr());
 
-let txt5 = "я люблю JS";
+// Создайте глобальную функцию getObj(), которая возвращает this.
+// А у каждого из объектов city1 или city2 метод getCity, который
+// ссылается на getObj. Проверьте работу метода. Примечание: к объекту
+// вызова можно обратиться через this..
 
-console.log(txt5.split(" "));
+function getObj() {
+	return this;
+}
+city1.getCity = getObj;
+city2.getCity = getObj;
 
-// Дана строка 'привет мир'. С помощью метода split
-// запишите каждый символ этой строки в отдельный элемент
-// массива.
+console.log(city1.getCity().getName());
+console.log(city2.getCity().getName());
 
-let txt6 = "привет мир";
+// Создайте массив d1 с числовыми величинами 45,78,10,3.
+// Добавьте в массив d1 еще одно число ( d1[7] = 100).
+// Выведите в консоль весь массив и его элементы с индексами 6 и 7.
 
-console.log(txt6.split(""));
+let d1 = [45, 78, 10, 3];
+
+d1[7] = 100;
+
+console.log(d1);
+console.log(d1[6], d1[7]);
+
+// Создайте массив d2 с числовыми величинами 45,78,10,3.
+// Посчитайте в переменную sum2 сумму чисел в нем, при помощи цикла for.
+
+let d2 = [45, 78, 10, 3];
+
+let sum2 = 0;
+
+for (let i = 0; i < d2.length; i++) {
+	sum2 += d2[i];
+}
+
+console.log(sum2);
+
+// Создайте массив d3 с числовыми величинами 45,78,10,3.
+// Добавьте в массив d3 еще одно число (например, d[7] = 100)
+// Посчитайте в переменную sum3 сумму чисел в нем, при помощи цикла for/in.
+
+let d3 = [45, 78, 10, 3];
+d3[7] = 100;
+let sum3 = 0;
+
+for (let item in d3) {
+	sum3 += d3[item];
+}
+console.log(sum3);
+
+// Создайте массив d4 с числовыми величинами 45,78,10,3.
+// Напишите функцию сортировки my(a,b), которая при вызове d4.sort(my)
+// отсортирует элементы массива по убыванию чисел. Вызовите d4.sort(my)
+
+let d4 = [45, 78, 10, 3];
+
+d4.sort((a, b) => b - a);
+console.log(d4);
