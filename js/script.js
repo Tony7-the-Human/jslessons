c089c4a7eee5893306aacb7519e8d255f51a1e48
Tony7-the-Human <mrfox88@yandex.ru>
2021-11-02 18:50:57 +0300

    add homework lesson 32

--- js/script.js
+++ js/script.js
@@ -30,4 +30,50 @@ window.addEventListener("DOMContentLoaded", () => {
 			});
 		}
 	});
+
+	// timer
+
+	const timeEnd = "2021-11-24";
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date());
+
+		const days = Math.floor(t / (1000 * 60 * 60 * 24));
+		const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+		const minutes = Math.floor((t / 1000 / 60) % 60);
+		const seconds = Math.floor((t / 1000) % 60);
+
+		return {
+			total: t,
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds,
+		};
+	}
+
+	// getTimeRemaining();
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector);
+		const days = document.querySelector("#days");
+		const hours = document.querySelector("#hours");
+		const minutes = document.querySelector("#minutes");
+		const seconds = document.querySelector("#seconds");
+		const timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+			days.innerHTML = t.days;
+			hours.innerHTML = t.hours;
+			minutes.innerHTML = t.minutes;
+			seconds.innerHTML = t.seconds;
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock(".timer", timeEnd);
 });
